090041d1cc4fa55bfd88ea9edfd93e0ed3c24f4a
joshuatrefzer <121818554+joshuatrefzer@users.noreply.github.com>
2025-02-22 16:28:00 +0100

    initial commit

---
+++ src/main/java/com/example/faq_backend_api/FaqBackendApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.faq_backend_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaqBackendApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FaqBackendApiApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/faq_backend_api/api/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.example.faq_backend_api.api.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.faq_backend_api.api.model.User;
+import com.example.faq_backend_api.service.UserService;
+
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@RestController
+public class UserController {
+
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/user")
+    public ResponseEntity<User> getUser(@RequestParam Integer id) {
+        return userService.getUser(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+}
